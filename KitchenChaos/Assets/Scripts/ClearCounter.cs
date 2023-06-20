d1d8a4c4dea1c23aec87c816944d0a35b1c987b5
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-20 12:46:52 +0300

    Added Parent to KitchenObject
    -> Counter has the information of what KitchenObject it has
    -> KitchenObject has the information what counter is it on(only counters for now)
    -> KitchenObject is responsible for setting its parent(counter or x(to be implemented)) and clearing its information from old counter

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
-{
+public class ClearCounter : MonoBehaviour {
+
+
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
-    public void Interact() {
-        Debug.Log("Interact");
-        Transform kitchenObjectTransform =  Instantiate(kitchenObjectSO.GetPrefab(), counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+    [SerializeField] private ClearCounter secondCounter;
+    [SerializeField] private bool testing;
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+
+    private KitchenObject kitchenObject;
+
+    private void Update() {
+        if (testing  && Input.GetKeyDown(KeyCode.T)) {
+            Debug.Log("How many times did keycode have been called?");
+            if (kitchenObject != null) {
+                if (kitchenObject.GetClearCounter().Equals(this)) {
+                    Debug.Log("Seeting parent to :" + secondCounter);
+                    kitchenObject.SetClearCounter(secondCounter);
+
+                }
+
+            }
+
+        }
     }
 
 
+    public void Interact() {
+        if (kitchenObject == null) {
+            // Has no object on it.
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.GetPrefab(), counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject !=null;
+    }
 }
