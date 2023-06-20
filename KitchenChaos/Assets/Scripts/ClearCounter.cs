7c02107067702697825c00978d84995a0ba8a8d2
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-20 10:22:34 +0300

    Added Events
    ->The counter which player faces now is highlighted using a visualEffect in counter prefab.
    -> All SelectedCounterVisuals inside ClearCounters are listening to the event Player will fire.Then activate selected visual if CurrentCounter equals selectedCounter
    ->  Made Player singleton(will change it when making multiplayer)

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact() {
         Debug.Log("Interact");
+        Transform kitchenObject =  Instantiate(tomatoPrefab, counterTopPoint);
+        kitchenObject.localPosition = Vector3.zero;
     }
 
 
--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -6,12 +7,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedCounterChangeEventArgs> OnSelectedCounterChange;
+
+    public class OnSelectedCounterChangeEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("There is more than one player instance");
+        }
+            Instance = this;
+    }
+
+    private void Start() {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        if (selectedCounter != null) {
+            selectedCounter.Interact();
+        }
+    }
 
     private void Update() {
         HandleMovement();
@@ -24,22 +52,27 @@ public class Player : MonoBehaviour {
 
     private void HandleInteractions() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        if(moveDir != Vector3.zero) {
+        if (moveDir != Vector3.zero) {
             lastInteractDir = moveDir;
         }
         float interactDistance = 2f;
-        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit rayCastHit , interactDistance, counterLayerMask)) {
-            if(rayCastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit rayCastHit, interactDistance, counterLayerMask)) {
+            if (rayCastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
                 // Has ClearCounter 
-                clearCounter.Interact();
+                //clearCounter.Interact();
+                if (clearCounter != selectedCounter) {
+                    SetSelectedCounter(clearCounter);
+                }
             }
-
-            if(rayCastHit.transform.GetComponent<ClearCounter>() != null) { }
-        }// could've written 2f directly instead interact dsitance
-                                                                        // -> these hard coding number is called magic numbers.DONT USE MAGIC NUMBERS
+            else {
+                SetSelectedCounter(null);
+            }
+        }
+        else {
+            SetSelectedCounter(null);
+        }
     }
 
     private void HandleMovement() {
@@ -89,5 +122,13 @@ public class Player : MonoBehaviour {
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChange?.Invoke(this, new OnSelectedCounterChangeEventArgs {
+            selectedCounter = selectedCounter
+        });
     }
 }
