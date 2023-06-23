4c1ffa89cc8f20166916867e53a545b7fe49ae52
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-23 12:27:20 +0300

    Added LookAtCamera.cs
    ->Making CuttingCounter cut progressBar look at camera

---
+++ KitchenChaos/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    private enum Mode {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted,
+    }
+
+    [SerializeField] private Mode mode;
+
+    private Camera mainCamera;
+    private Transform player;
+
+    private void Start() {
+        mainCamera = Camera.main;
+        player = FindObjectOfType<Player>().transform;
+    }
+
+    private void LateUpdate() {
+        switch (mode) {
+            case Mode.LookAt:
+                transform.LookAt(mainCamera.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 dirFromCamera = transform.position - mainCamera.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = mainCamera.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -mainCamera.transform.forward;
+                break;
+        }
+    }
+
+}
